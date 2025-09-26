555d6cf9856b0ecc9345304fe3a26fba64f684e1
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-26 15:11:33 +0600

    file rename

---
+++ convert-speed.cpp
@@ -0,0 +1,8 @@
+// ================================================
+//                Platform : CodeChef
+//                Contest  : Practice (Beginner)
+//                Problem  : Time Conversion
+//                Language : C++
+//                Topic    : [Basic-Math, Time-Conversion]
+//                File     : convert-speed.cpp
+// ================================================
